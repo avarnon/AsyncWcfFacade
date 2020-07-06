89337d452b44f81a7288960bb00fe12aa539119e
Andrew Varnon <avarnon@rightpoint.com>
2020-07-06 12:51:59 -0400

    .NET Core SOAP example

---
+++ CoreWcfFacade/Services/FacadeService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CoreWcfFacade.Services
+{
+    public class FacadeService : IFacadeService
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public FacadeService(IHttpClientFactory clientFactory)
+        {
+            this._clientFactory = clientFactory ?? throw new ArgumentNullException(nameof(clientFactory));
+        }
+
+        public async Task<string> Search(string term) => await this._clientFactory.CreateClient().GetStringAsync($"search?q={HttpUtility.UrlEncode(term ?? string.Empty)}");
+    }
+}
---
+++ CoreWcfFacade/Services/IFacadeService.cs
@@ -0,0 +1,12 @@
+﻿using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace CoreWcfFacade.Services
+{
+    [ServiceContract]
+    public interface IFacadeService
+    {
+        [OperationContract]
+        Task<string> Search(string term);
+    }
+}
